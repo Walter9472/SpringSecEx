572e32ae720d7bab11a10a6c09c380c7848bc62d
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-23 12:33:59 +0200

    Add initial Spring Boot application with custom security configuration and student management

---
+++ src/main/java/com/walter/SpringSecEx/HelloController.java
@@ -0,0 +1,15 @@
+package com.walter.SpringSecEx;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome To Walter " + request.getSession().getId();
+
+    }
+}
